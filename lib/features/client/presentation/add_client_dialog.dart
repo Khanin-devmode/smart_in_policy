05ff3cc1b208aadd36cc1454b917419395548094
Khanin <khanin.dev@gmail.com>
2023-06-17 22:16:48 +0700

    refactor dialog and wrap with stateless widget

---
+++ lib/features/client/presentation/add_client_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AddClientDialog extends StatelessWidget {
+  const AddClientDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Basic dialog title'),
+      content: const Text(
+        'A dialog is a type of modal window that\n'
+        'appears in front of app content to\n'
+        'provide critical information, or prompt\n'
+        'for a decision to be made.',
+      ),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
+          child: const Text('Disable'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
+          child: const Text('Enable'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/features/client/presentation/clients_page.dart
+++ lib/features/client/presentation/clients_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_in_policy/features/client/presentation/add_client_dialog.dart';
 
 class ClientsPage extends StatefulWidget {
   const ClientsPage({super.key});
@@ -14,8 +15,20 @@ class _ClientsPageState extends State<ClientsPage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Clients'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () => _dialogBuilder(context), icon: Icon(Icons.add))
+        ],
       ),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return const AddClientDialog();
+      },
     );
   }
 }
